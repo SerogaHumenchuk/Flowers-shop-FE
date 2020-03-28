01afe285e17c0f33df51aa1d0a53f1d7e8e28c81
Serhii Humenchuk <serhii.humenchuk-external@sema4genomics.com>
2020-03-28 20:27:45 +0200

    removed unused images

--- src/configs/listOfGoods/viola/images/index.js
+++ src/configs/listOfGoods/viola/images/index.js
@@ -1,42 +1,24 @@
-// import viola1 from './viola1.jpg';
-// import viola2 from './viola2.jpg';
-// import viola3 from './viola3.jpg';
 import viola4 from './viola4.jpg';
-// import viola5 from './viola5.jpg';
-// import viola6 from './viola6.jpg';
-// import viola7 from './viola7.jpg';
 import viola8 from './viola8.jpg';
-// import viola9 from './viola9.jpg';
-// import viola10 from './viola10.jpg';
-// import viola11 from './viola11.jpg';
-// import viola12 from './viola12.jpg';
-// import viola13 from './viola13.jpg';
-// import viola14 from './viola14.jpg';
 import viola15 from './viola15.jpg';
 import viola16 from './viola16.jpg';
 import viola17 from './viola17.jpg';
-// import viola18 from './viola18.jpg';
-// import viola19 from './viola19.jpg';
 import viola20 from './viola20.jpg';
 import viola21 from './viola21.jpg';
 import viola22 from './viola22.jpg';
-// import viola23 from './viola23.jpg';
 import viola24 from './viola24.jpg';
 import viola25 from './viola25.jpg';
 import viola26 from './viola26.jpg';
 import viola27 from './viola27.jpg';
-// import viola28 from './viola28.jpg';
 import viola29 from './viola29.jpg';
 import viola30 from './viola30.jpg';
 import viola31 from './viola31.jpg';
 import viola32 from './viola32.jpg';
-// import viola34 from './viola34.jpg';
 import viola35 from './viola35.jpg';
 import viola36 from './viola36.jpg';
 import viola37 from './viola37.jpg';
 import viola38 from './viola38.jpg';
 import viola39 from './viola39.jpg';
-// import viola40 from './viola40.jpg';
 
 export const vioalImages = [
   viola36,
@@ -45,19 +27,11 @@ export const vioalImages = [
   viola31,
   viola38,
   viola21,
-  // viola1,
-  // viola18,
-  // viola2,
   viola27,
   viola29,
   viola22,
-  // viola34,
   viola17,
   viola35,
-  // viola11,
-  // viola28,
-  // viola3,
-  // viola19,
   viola20,
   viola25,
   viola39,
@@ -66,16 +40,6 @@ export const vioalImages = [
   viola26,
   viola15,
   viola32,
-  // viola5,
-  // viola6,
   viola37,
-  // viola7,
-  // viola13,
   viola8,
-  // viola9,
-  // viola40,
-  // viola10,
-  // viola12,
-  // viola23,
-  // viola14,
 ];
