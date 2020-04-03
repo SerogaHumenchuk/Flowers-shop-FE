fdf8b7c149da91bda23e6ab5fcdcf942ca7403ff
Serhii Humenchuk <serhii.humenchuk-external@sema4genomics.com>
2020-04-04 02:04:14 +0300

    removed unsed fields from store

--- src/redux/General/reducers/index.js
+++ src/redux/General/reducers/index.js
@@ -1,23 +1,11 @@
 import { generalActionTypes } from '../actionTypes';
 
 const initialState = {
-  gridType: 'cards', // cards, gallery
-  title: {title: 'Троянди', price: '35-40 грн'},
   isOpenDrawer: true,
 };
 
 export const generalReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case generalActionTypes.updateGridType:
-      return {
-        ...state,
-        gridType: payload,
-      };
-    case generalActionTypes.updateTitle:
-      return {
-        ...state,
-        title: payload,
-      };
     case generalActionTypes.toggleDrawer:
       return {
         ...state,
